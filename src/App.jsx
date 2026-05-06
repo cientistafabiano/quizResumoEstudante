5f278cdddf44b10c9789d0690db83bb3f8a30fd6
cientistafabiano <fabiano.dev.full@gmail.com>
2026-05-05 21:57:28 -0300

    mostra todas as noticias

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="flex flex-col h-screen">
       <h1 className='text-red-500 text-5xl font-bold'>Quiz</h1>
+      <h1 className='text-red-500 text-5xl font-bold'>Noticias</h1>
       <main className='flex-1 flex items-center justify-center'>
         <ProductCard noticiaText={noticiaText}/>
       </main>
