c04b5b1887564bb4a259c584c7dce922394c0de2
cientistafabiano <fabiano.dev.full@gmail.com>
2026-05-12 23:01:20 -0300

    criando a prop onAnswer

--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -1,9 +1,10 @@
 //import {Card} from './Card.jsx';
 import React from "react";
 
-export default function ProductCard({ data }) {
+export default function ProductCard({ data, onAnswer }) {
     {/*criando as variaveis que recebem os dados de data */}
     const { pergunta, opcoes, resposta_correta } = data;
+    const {tema, titulo, resumo, conteudo_completo} = data;
     return (
         <div className="bg-gray-800 p-6 rounded-2xl shadow-lg w-full 
             max-w-xl border border-gray-700"> 
@@ -14,12 +15,15 @@ export default function ProductCard({ data }) {
                     <button 
                         className='text-left px-4 py-3 cursor-pointer
                         bg-blue-800 rounded-lg text-white'
-                        key={index}>{opcao}
+                        key={index}
+                        onClick={() => onAnswer(opcoes)}  >  
+                        {opcao}
                     </button>
                 ))}
             </div>
         
-        </div>
+        </div>        
+       
     );
 }
 
